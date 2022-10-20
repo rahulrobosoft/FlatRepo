6c57e39b53b43c50a78202dbe7be7dfcc804bbd7
rahulrobosoft <rahul.robosoftin.com>
2022-10-20 18:09:31 +0530

    home

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PlaneHeaderComponent } from './plane-header/plane-header.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { HomeComponent } from './home/home.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PlaneHeaderComponent
+    PlaneHeaderComponent,
+    ForgotComponent,
+    HomeComponent,
+    HomeHeaderComponent
   ],
   imports: [
     BrowserModule,
